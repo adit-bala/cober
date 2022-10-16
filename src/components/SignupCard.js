10e42a3cae510a4fdd1b22b4136c016196620abb
adit-bala <adityarbala@gmail.com>
2022-10-15 23:21:31 -0700

    get values on submit for `SignUp`

--- src/components/SignupCard.js
+++ src/components/SignupCard.js
@@ -3,6 +3,7 @@ import {
   Box,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Select,
   Input,
   InputGroup,
@@ -15,112 +16,121 @@ import {
   useColorModeValue,
   Link,
 } from '@chakra-ui/react';
-import { Formik } from 'formik';
+import { Formik, Field, Form } from 'formik';
 
 export default function SignUpCard() {
   return (
-    <Formik
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-    >
-      {props => (
-        <Flex
-          minH={'100vh'}
-          align={'center'}
-          justify={'center'}
-          bg={'gray.50'}
-        >
-          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-            <Stack align={'center'}>
-              <Heading fontSize={'4xl'} textAlign={'center'}>
-                Sign up
-              </Heading>
-              <Text fontSize={'lg'} color={'gray.600'}>
-                to ride with fellow students ✌️
-              </Text>
-            </Stack>
-            <Box
-              rounded={'lg'}
-              bg={'white'}
-              boxShadow={'lg'}
-              p={8}
-            >
-              <Stack spacing={4}>
-                <FormControl id="pfp" isRequired>
-                  <FormLabel>Profile Picture</FormLabel>
-                  <Input type="file" />
-                </FormControl>
-                <HStack>
-                  <Box>
-                    <FormControl id="firstName" isRequired>
-                      <FormLabel>First Name</FormLabel>
-                      <Input type="text" />
-                    </FormControl>
-                  </Box>
-                  <Box>
-                    <FormControl id="lastName">
-                      <FormLabel>Last Name</FormLabel>
-                      <Input type="text" />
-                    </FormControl>
-                  </Box>
-                </HStack>
-                <FormControl id="email" isRequired>
-                  <FormLabel>Email address (must end in .edu) </FormLabel>
-                  <Input type="email" />
-                </FormControl>
-                <FormControl id="password" isRequired>
-                  <FormLabel>Password</FormLabel>
-                  <InputGroup>
-                    <Input type={'text'} />
-                    <InputRightElement h={'full'}>
-                      <Button variant={'ghost'}></Button>
-                    </InputRightElement>
-                  </InputGroup>
-                </FormControl>
-                <FormControl>
-                  <FormLabel>Gender</FormLabel>
-                  <Select placeholder="Select your gender">
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
-                  </Select>
-                </FormControl>
-
-                <Stack spacing={10} pt={2}>
-                  <Button
-                    loadingText="Submitting"
-                    size="lg"
-                    bg={'orange.300'}
-                    color={'white'}
-                    _hover={{
-                      bg: 'orange.500',
-                    }}
-                  >
-                    Sign up
-                    {/* <Button
-                  isLoading
-                  loadingText='Submitting'
-                  colorScheme='teal'
-                  variant='outline'
-                >
-                  Submit
-                </Button> */}
-                  </Button>
-                </Stack>
-                <Stack pt={6}>
-                  <Text align={'center'}>
-                    Already a user? <Link color={'orange.300'}>Login</Link>
-                  </Text>
-                </Stack>
-              </Stack>
-            </Box>
+    <>
+      <Flex minH={'100vh'} align={'center'} justify={'center'} bg={'gray.50'}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'} textAlign={'center'}>
+              Sign up
+            </Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              to ride with fellow students :v:
+            </Text>
           </Stack>
-        </Flex>
-      )}
-    </Formik>
+          <Box rounded={'lg'} bg={'white'} boxShadow={'lg'} p={8}>
+            <Stack spacing={4}>
+              <Formik
+                initialValues={{
+                  firstName: 'Adit',
+                  lastName: 'Bala',
+                  email: 'cober@berkeley.edu',
+                  password: 'ilovecoding<3',
+                  Gender: 'Male',
+                }}
+                onSubmit={(values, actions) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                  }, 1000);
+                }}
+              >
+                {props => (
+                  <Form>
+                    <HStack>
+                      <Box>
+                        <Field name="firstName">
+                          {({ field, form }) => (
+                            <FormControl>
+                              <FormLabel>First name</FormLabel>
+                              <Input {...field} placeholder="name" />
+                              <FormErrorMessage>
+                                {form.errors.name}
+                              </FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                        <Field name="lastName">
+                          {({ field, form }) => (
+                            <FormControl>
+                              <FormLabel>Last name</FormLabel>
+                              <Input {...field} placeholder="name" />
+                              <FormErrorMessage>
+                                {form.errors.name}
+                              </FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </Box>
+                    </HStack>
+                    <Field name="email">
+                      {({ field, form }) => (
+                        <FormControl>
+                          <FormLabel>Email (must end in .edu) </FormLabel>
+                          <Input {...field} placeholder="name" />
+                          <FormErrorMessage>
+                            {form.errors.name}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="password">
+                      {({ field, form }) => (
+                        <FormControl>
+                          <FormLabel>Password </FormLabel>
+                          <Input {...field} placeholder="name" />
+                          <FormErrorMessage>
+                            {form.errors.name}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="Gender">
+                      {({ field, form }) => (
+                        <FormControl>
+                          <FormLabel>Gender </FormLabel>
+                          <Select {...field} placeholder="Select your gender">
+                            <option>Male</option>
+                            <option>Female</option>
+                            <option>Other</option>
+                          </Select>
+
+                          <FormErrorMessage>
+                            {form.errors.name}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Stack spacing={10} pt={2}>
+                      <Button
+                        mt={4}
+                        colorScheme="teal"
+                        isLoading={props.isSubmitting}
+                        type="submit"
+                      >
+                        Submit
+                      </Button>
+                    </Stack>
+                  </Form>
+                )}
+              </Formik>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   );
 }
