f84b7d759d8f0f954e0fef10e7dcff3707701c58
kellyyyeh <pegasusyeh@gmail.com>
2022-10-15 20:32:43 -0700

    buttons without logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,36 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  color,
+  Container,
+  Flex,
+  HStack,
+  IconButton,
+  Image,
+  Spacer,
+  useBreakpointValue,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import * as React from 'react'
+import { FiMenu } from 'react-icons/fi'
+// import { Logo } from './Logo'
 
 export default function Nav() {
   return (
-    <h1>Hello</h1>
+    <Box>
+      <Flex display="flex" margin="20px" justify="flex-end">
+        <ButtonGroup variant="ghost">
+          <Button>Home</Button>
+          <Button>About</Button>
+          <Button variant="solid"
+            backgroundColor="#204988"
+            textColor="white"
+            >
+              Sign In
+          </Button>
+        </ButtonGroup>
+      </Flex>
+    </Box>
   )
 }
