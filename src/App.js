3e4ceec08a1dcac9b4067bcda1b8fd4110344edf
angtu <Angelinet1030@gmail.com>
2022-10-16 10:40:27 -0700

    About

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Home from './components/Home';
 import MyRides from './components/MyRides';
 import RequestCard from './components/RequestCard';
 import Requests from './components/Requests';
-import SignUpCard from './components/SignUpCard';
+import SignUpCard from './components/SignupCard';
 
 function App() {
   const [page, setPage] = useState(<Home />);
